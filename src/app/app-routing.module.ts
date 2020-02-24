d52050ce5ecdcc56c787bbe4262de3e21be56402
aqiu384 <aqiu384@gmail.com>
2020-02-24 14:31:19 -0800

    switch to hash links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -175,7 +175,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(appRoutes) ],
+  imports: [ RouterModule.forRoot(appRoutes, { useHash: true }) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
