b9cc1a03a7bf39accdf78e59271d33d00c0ff577
aqiu384 <aqiu384@gmail.com>
2024-05-20 17:13:59 -0700

    pq skill pwr init

--- src/app/pq/compendium.module.ts
+++ src/app/pq/compendium.module.ts
@@ -21,10 +21,22 @@ import DEMON_CODES_JSON from './data/demon-codes.json';
 import SKILL_CODES_JSON from './data/skill-codes.json';
 import DEMON_UNLOCKS_JSON from './data/demon-unlocks.json';
 
+function skillRowToString(row: (string | number)[]): string {
+  const [power, minHits, maxHits, accuracy, mod, effect, cond] = row.slice(5);
+  const baseMod = parseInt(mod.toString());
+  const powerStr = power === 0 ? '' : `${power} pwr`;
+  const hitStr = minHits !== maxHits ? `${minHits}-${maxHits} hits` : maxHits === 0 ? '' : `${maxHits} hits`;
+  const modStr = baseMod === 0 ? '' : baseMod < 1000 ? `${mod}%` : baseMod < 2000 ? `x${(baseMod - 1000) / 100}` : baseMod < 3000 ? `+${baseMod - 2000}%` : `+${baseMod - 3000}`;
+  const effectStr = `${modStr} ${effect === '-' ? '' : effect} ${cond === '-' ? '' : cond}`.trim();
+  const fullStr = [powerStr, hitStr, effectStr].filter(s => s !== '').join(', ');
+  return fullStr.substring(0, 1) === 'x' ? fullStr : fullStr.substring(0, 1).toUpperCase() + fullStr.substring(1);
+}
+
 function createCompConfig(): CompendiumConfig {
   const resistElems = COMP_CONFIG_JSON.resistElems;
   const skillElems = resistElems.concat(COMP_CONFIG_JSON.skillElems);
   const races = [];
+  const skillData = {};
   const inheritTypes: { [elem: string]: number } = {};
 
   for(const race of COMP_CONFIG_JSON.races) {
@@ -46,17 +58,22 @@ function createCompConfig(): CompendiumConfig {
   }
 
   const COST_HP = 1 << 10;
-  const COST_MP = 3 << 10;
-  const COST_CC = 15 << 10;
+  const COST_MP = (3 << 10) - 1000;
+  const COST_CC = (15 << 10) - 2000;
 
-  for (const entry of Object.values(SKILL_DATA_JSON)) {
-    const cost = entry['cost'];
-    const costType = cost > 1000 ? (cost > 2000 ? COST_CC - 2000 : COST_MP - 1000) : COST_HP - 200;
-    entry['cost'] = cost ? cost + costType: 0;
+  for (const row of Object.values(SKILL_DATA_JSON)) {
+    const [sname, elem, target, rank, baseCost, power, minHits, maxHits, accuracy, mod, effect, cond] = row;
+    const cost = parseInt(baseCost.toString())
+    skillData[sname] = {
+      elem,
+      target: target === '-' ? 'Self' : target,
+      cost: cost ? cost + (cost > 1000 ? (cost > 2000 ? COST_CC : COST_MP) : COST_HP) : 0,
+      effect: skillRowToString(row)
+    }
   }
 
   for (const [code, name] of Object.entries(SKILL_CODES_JSON)) {
-    SKILL_DATA_JSON[name]['code'] = parseInt(code, 10);
+    skillData[name]['code'] = parseInt(code, 10);
   }
 
   for (const demon of Object.values(DEMON_DATA_JSON)) {
@@ -84,7 +101,7 @@ function createCompConfig(): CompendiumConfig {
     raceOrder: races.reduce((acc, x, i) => { acc[x] = i; return acc }, {}),
     appCssClasses: ['pq1'],
 
-    skillData: SKILL_DATA_JSON,
+    skillData,
     skillElems,
     ailmentElems: COMP_CONFIG_JSON.ailments,
     elemOrder: skillElems.reduce((acc, x, i) => { acc[x] = i; return acc }, {}),
